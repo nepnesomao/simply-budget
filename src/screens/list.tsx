e15103b619e1ff5baa6ee3ebeb7e92e50a47e65c
Ömer KÜLAHLI <nepnesomao@gmail.com>
2022-02-28 17:10:56 +0300

    osman

--- src/screens/list.tsx
+++ src/screens/list.tsx
@@ -1,25 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import {  StyleSheet } from 'react-native';
 import { Box, Center, Text, VStack} from 'native-base'
 import BudgetList from '../components/budget-list'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import shortid from 'shortid'
 
 const BUDGET_ITEMS = 'budget-items'
-const initialData = [
-    {
-        id: shortid.generate(),
-        date: "01/01/2022",
-        type: "Market",
-        price:"150"
-    },
-    {
-        id: shortid.generate(),
-        date: "02/01/2022",
-        type: "Market",
-        price:"110"
-    }
-]
 
 const ListScreen = () => {
     const [data, setData] = useState([])
@@ -44,7 +28,7 @@ const ListScreen = () => {
         <Center _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} flex={1}>
         <VStack space={1} p={4} alignItems="center" mt="-20px" w="full">
             <Box style={{borderBottomWidth:1, width:'100%', paddingLeft:'15%'}}>
-                <Text>Budget List</Text>
+                <Text>Harcamalarim</Text>
             </Box>
             <BudgetList
                 data={data}
